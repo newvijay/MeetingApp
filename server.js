321c1a26201568dd7de33fe870c25c0fa180fc72
Vijay <laasyakuppam@laasyas-mbp.home>
2020-10-19 21:56:11 -0400

    updated port

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const { v4: uuidV4 } = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer (server,{
+  debug:true
+});
+
+app.use('/peerjs',peerServer);
+
 app.get("/", (req, res) => {
   res.render("index");
 });
